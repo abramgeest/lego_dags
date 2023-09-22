95d5674390c9803d51b7adec39feec830c61bf53
Abram Van Der Geest <abram.ghost@gmail.com>
2023-09-22 10:06:38 -0400

    including aws as provider

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.20.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+
 data "aws_caller_identity" "current" {}
 
 locals {
